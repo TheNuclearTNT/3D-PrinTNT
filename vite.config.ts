03cbc3ef01793d4a7960310f941324ced47cdc0b
TheNuclearTNT <69483655+TheNuclearTNT@users.noreply.github.com>
2025-10-05 15:09:26 -0400

    Deploy Fix 1.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/3D-PrinTNT/",
   server: {
     host: "::",
     port: 8080,
